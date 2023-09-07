0d4f48199d3af4e71e973dbff5fafb67ab840588
leivaedgar <edgar.leiva@slayte.com>
2023-09-07 06:53:16 -0600

    Resources and pages pt1

--- pages/share.js
+++ pages/share.js
@@ -1,17 +1,19 @@
 import { toast } from 'react-toastify';
 
-export function shareArticle() {
-    if (typeof window !== 'undefined') {
-        const url = window.location.href;
-        navigator.clipboard.writeText(url);
-        console.log("shareArticle function triggered");
 
-        if (!toast.isActive('copyToast')) {
-            toast.success("Article link copied to clipboard!", {
-                autoClose: 2000,
-                hideProgressBar: true,
-                toastId: 'copyLink'
-            });
-        }
-    }
+export function shareArticle() {
+if (typeof window !== 'undefined') {
+const url = window.location.href;
+navigator.clipboard.writeText(url);
+console.log("shareArticle function triggered");
+
+
+if (!toast.isActive('copyToast')) {
+toast.success("Article link copied to clipboard!", {
+autoClose: 2000,
+hideProgressBar: true,
+toastId: 'copyLink'
+});
+}
+}
 }
